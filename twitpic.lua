b87568993b4c2c98255902376716f42966a14a97
Arkiver2 <Arkiver@hotmail.com>
2014-09-05 17:16:11 +0200

    twitpic.lua: show all lines

--- twitpic.lua
+++ twitpic.lua
@@ -134,7 +134,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
   local status_code = http_stat["statcode"]
   
   url_count = url_count + 1
-  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. ".  \r")
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. ".  \n")
   io.stdout:flush()
   if status_code >= 500 or
     (status_code >= 400 and status_code ~= 404 and status_code ~= 403) then
