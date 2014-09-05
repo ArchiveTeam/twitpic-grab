0d3da31bcaf16df2c79a7fd2f2123f874b6a0b39
David Yip <yipdw@member.fsf.org>
2014-09-05 10:41:56 -0500

    Failure server: use tai64n timestamps in document IDs.
    Why? I have an irrational dislike of periods in my document IDs.

--- util/failure-server/server.rb
+++ util/failure-server/server.rb
@@ -11,7 +11,7 @@ post '/fail' do
   downloader = params[:downloader]
   item_name = params[:item_name]
 
-  id = "#{downloader}:#{item_name}:#{Time.now.to_f}"
+  id = "#{downloader}:#{item_name}:#{Time.now.tai64n}"
 
   resp = db.put(id, {
     downloader: downloader,
@@ -21,4 +21,37 @@ post '/fail' do
   }, credentials)
 
   status resp.code
+end
+
+# libtai64-ruby
+# http://cr.yp.to/libtai/tai64.html
+# Further stolen from https://gist.github.com/2983/78cbbfcc1f1dca646aff100e94e29188b5390a5a
+class Time
+  TAI64_REGEX = Regexp.new(/(?:^\@)?([0-9a-fA-F]{16})/)
+  TAI64N_REGEX = Regexp.new(/#{TAI64_REGEX}([0-9a-fA-F]{8})/)
+
+  def self.tai64(str, leapseconds=10)
+    if match = TAI64N_REGEX.match(str).to_a.values_at(1)
+      tai64 = match[0].hex - 2**62 - leapseconds
+      return Time.at(tai64)
+    end
+    raise ArgumentError, "not TAI64 compliant date: #{str}"
+  end
+
+  def self.tai64n(str, leapseconds=10)
+    if match = TAI64N_REGEX.match(str).to_a.values_at(1,2)
+      tai64 = match[0].hex - 2**62 - leapseconds
+      nano = match[1].hex / 10**3
+      return Time.at(tai64,nano)
+    end
+    raise ArgumentError, "not TAI64N compliant date: #{str}"
+  end
+
+  def tai64(leapseconds=10)
+    return sprintf("%016x", 2**62 + self.to_i + leapseconds)
+  end
+
+  def tai64n(leapseconds=10,nanosec=500)
+    return sprintf("%016x%08x", 2**62 + self.to_i + leapseconds, self.usec * 10**3 + nanosec)
+  end
 end
