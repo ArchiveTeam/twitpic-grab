dfee59342965b882b74eb234d1ec36b5323cab2b
Arkiver2 <Arkiver@hotmail.com>
2014-09-06 12:56:27 +0200

    twitpic.lua: comments fix

--- twitpic.lua
+++ twitpic.lua
@@ -31,7 +31,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     
     local twitpicurl = "twitpic%.com/"..item_value
     
-    if string.match(url, twitpicurl) then
+    if string.match(url, item_value) then
       html = read_file(file)
       
       for commentid in string.gmatch(html, '<div class="[^"]+" data-id="([0-9]+)">') do
