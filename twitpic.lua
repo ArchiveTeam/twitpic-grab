d49e0b638cd87c90a87b60107a07d28d73ee69ca
Arkiver2 <Arkiver@hotmail.com>
2014-09-06 12:14:38 +0200

    twitpic.lua: fix typo

--- twitpic.lua
+++ twitpic.lua
@@ -66,7 +66,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
       
       for prevpage in string.gmatch(html, '<div class="left">[^<]+<a href="(%?[^"]+)">[^<]+</a>[^<]+</div>') do
         local prevpageurl = twitpicurl.."/"..prevpage
-        table.insert(ursl, { url=prevpageurl })
+        table.insert(urls, { url=prevpageurl })
       end
     end
   end
