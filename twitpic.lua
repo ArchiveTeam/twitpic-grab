8ccf3e95b290ecc77fe0faa25f9d37b6bb27f0bc
Arkiver2 <Arkiver@hotmail.com>
2014-09-06 12:00:45 +0200

    twitpic.lua always load as html

--- twitpic.lua
+++ twitpic.lua
@@ -32,9 +32,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     twitpicurl = "twitpic%.com/"..item_value
     
     if string.match(url, twitpicurl) then
-      if not html then
-        html = read_file(file)
-      end
+      html = read_file(file)
       
       for videourl in string.gmatch(html, '<meta name="twitter:player:stream" value="(http[^"]+)"') do
         table.insert(urls, { url=videourl })
