d7f905acf73da4d7693c9156a5cd363b34648521
Arkiver2 <Arkiver@hotmail.com>
2014-09-06 18:32:19 +0200

    pipeline.py: change USER_AGENT so tests can run

--- pipeline.py
+++ pipeline.py
@@ -58,7 +58,7 @@ if not WGET_LUA:
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
 VERSION = "20140906.05"
-USER_AGENT = 'ArchiveTeam'
+USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30618; MAXTHON 2.0)'
 TRACKER_ID = 'twitpic'
 TRACKER_HOST = 'tracker.archiveteam.org'
 
