175c8553c52b3cd1538ab1558819249fa51a0d21
Arkiver2 <Arkiver@hotmail.com>
2014-09-07 16:21:12 +0200

    twitpic.lua: fix for scraping event urls

--- twitpic.lua
+++ twitpic.lua
@@ -141,7 +141,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     if string.match(url, "twitpic%.com/event/[0-9a-zA-Z]+") then
       html = load_file(file)
       
-      for eventurl in string.gmatch(html, '<a href="(http[^/]+/e/[^"]+)">') do
+      for eventurl in string.gmatch(html, '<a href="(http[s]?://[^/]+/e/[^"]+)">') do
         table.insert(urls, { url=eventurl })
         for eventjson in string.gmatch(eventurl, "/e/([0-9a-zA-Z])") do
           table.insert(urls, { url=("http://api.twitpic.com/2/event/show.json?id="..eventjson) })
