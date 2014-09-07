118c1d5fcf5e8c3e23b87665875f31de905c396c
Arkiver2 <Arkiver@hotmail.com>
2014-09-07 10:49:16 +0200

    twitpic.lua: exclude every second tag page when
    last page is reached

--- twitpic.lua
+++ twitpic.lua
@@ -125,7 +125,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
           local page = string.match(url, "http://twitpic%.com/tag/[0-9a-zA-Z]+[/]?%?page=([0-9]+)")
           local tagid = string.match(url, "http://twitpic%.com/tag/([0-9a-zA-Z]+)[/]?%?page=[0-9]+")
           local prevpage = page - 1
-          local nextpage = page + 1
+          local nextpage = page + 2
           local prevurlslash = "http://twitpic.com/tag/"..tagid.."/?page="..prevpage
           local prevurl = "http://twitpic.com/tag/"..tagid.."?page="..prevpage
           local nexturlslash = "http://twitpic.com/tag/"..tagid.."/?page="..nextpage
