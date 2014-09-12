88c6da7adad2071028f173a6e41ce843af572695
Christopher Foo <chris.foo@gmail.com>
2014-09-12 15:31:28 -0400

    Random user agents and headers. Treat 403 from twitpic as problem. Bump version.

--- twitpic.lua
+++ twitpic.lua
@@ -288,9 +288,11 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
   if status_code >= 200 and status_code <= 399 then
     downloaded[url.url] = true
   end
-
+  
+  -- consider 403 as banned from twitpic, not pernament failure
   if status_code >= 500 or
-    (status_code >= 400 and status_code ~= 404 and status_code ~= 403) then
+    (status_code >= 400 and status_code ~= 404) or
+    (status_code == 403 and string.match(url["host"], "twitpic%.com")) then
     if string.match(url["host"], "twitpic%.com") or
       string.match(url["host"], "cloudfront%.net") or
       string.match(url["host"], "twimg%.com") or
