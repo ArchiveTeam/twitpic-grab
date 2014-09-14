5ea065fe3a3fee34d434b7ab7d19d28ad7a4c5b1
Christopher Foo <chris.foo@gmail.com>
2014-09-14 16:15:04 -0400

    twitpic.lua: Fix 403 logic bug where all 403s went into retry block. Bump ver.
    My bad :)

--- twitpic.lua
+++ twitpic.lua
@@ -256,7 +256,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
   
   -- consider 403 as banned from twitpic, not pernament failure
   if status_code >= 500 or
-    (status_code >= 400 and status_code ~= 404) or
+    (status_code >= 400 and status_code ~= 404 and status_code ~= 403) or
     (status_code == 403 and string.match(url["host"], "twitpic%.com")) then
     if string.match(url["host"], "twitpic%.com") then
       
