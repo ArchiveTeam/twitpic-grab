d0c58d55042147e1ba8d19300be446e79d4cd0bd
David Yip <yipdw@member.fsf.org>
2014-09-17 14:00:55 -0500

    Ignore Cloudfront URLs; bump version.

--- twitpic.lua
+++ twitpic.lua
@@ -217,9 +217,9 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
     if string.match(url, "/%%5C%%22") or
       string.match(url, '/[^"]+"') then
       return false
-    elseif string.match(url, "cloudfront%.net") or
-      string.match(url, "twimg%.com")  or
-      string.match(url, "amazonaws%.com") then
+    elseif string.match(url, "cloudfront%.net") then
+      return false
+    elseif string.match(url, "twimg%.com") or string.match(url, "amazonaws%.com") then
       return verdict
     elseif string.match(url, "advertise%.twitpic%.com") then
       return false
